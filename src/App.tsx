8cc967d6468cecc4ae0e92ff04d1195fea048879
johnwendell <johnwendell.watts@students.mq.edu.au>
2022-09-30 21:37:10 +1000

    Footer v1.0
    First iteration - requires feedback.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-
+// import React from "react";
+import Footer from './component/Footer';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
-    </div>
+      <Footer/>
+         </div>
   );
 }
 
