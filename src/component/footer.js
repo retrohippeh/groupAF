01c5726e9b7ed8199683b0d8406777ead7aee12a
johnwendell <johnwendell.watts@students.mq.edu.au>
2022-09-30 12:59:09 +1000

    Start on footer

---
+++ src/component/footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function  () {
+  return (
+    <div id="FOOTER-START">
+        <footer>
+<span className="footer-title">Socials</span>
+<div id="FOOTER-TOP">
+
+</div>
+<a></a>
+<div id="FOOTER-BOTTOM">
+
+</div>
+
+        </footer>
+         </div>
+  )
+}
